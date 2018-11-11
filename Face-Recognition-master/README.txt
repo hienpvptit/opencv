4e9561dab3cb9fa4c12501545e8dd45f3b472cdb
hienpvptit <36995026+hienpvptit@users.noreply.github.com>
2018-11-11 17:02:16 +0700

    Add files via upload

---
+++ Face-Recognition-master/README.txt
@@ -0,0 +1,18 @@
+### Require install
+* Python 2.x
+* OpenCV 2.x
+* Numpy
+
+## Running the tests
+
+* run the dataSetGenerator.py and enter a your name to create face samples with your face
+* run trainer.py to train faces
+* run detector.py to dectect faces
+
+## Authors
+
+* **[Anirban Kar](http://thecodacus.com/author/admin/)**
+
+## Editor
+
+* **[Hien Phan](http://github.com/hienpvptit) **
---
+++ Face-Recognition-master/dataSetGenerator.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.insert(0,'person')
+
+import cv2
+from person import *
+
+new_name = None
+while True:
+    new_name = str(input("Enter your name: ")).lower()
+    if person.append_data(new_name):
+        break
+
+new_id = person.person[0]["nums"]
+
+print "Start generator sample photo!"
+print "Please look at the camera until the stop message appears!"
+try:
+    cam = cv2.VideoCapture(0)
+    detector=cv2.CascadeClassifier('Classifiers/face.xml')
+    i = 0
+    offset = 50
+      
+    while True:
+        ret, im =cam.read()
+        gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('im', gray)
+        faces=detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+        for(x,y,w,h) in faces:
+            i=i+1
+            cv2.imwrite("dataSet/face-"+ str(new_id) +'.'+ str(i) + ".jpg", gray[y-offset:y+h+offset,x-offset:x+w+offset])
+            cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
+            #cv2.imshow('im',im[y-offset:y+h+offset,x-offset:x+w+offset])
+            print i*10/2,
+        if i>=20:
+            print "%   OK"
+            cam.release()
+            cv2.destroyAllWindows()
+            break
+except:
+    print "Error"
+    person.pop_data()
+    cam.release()
+    cv2.destroyAllWindows()
+
